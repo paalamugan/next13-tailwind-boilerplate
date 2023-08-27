759f09ccfd5efb20574838020a5ad4057f175139
paalamugan <paalamugan@gmail.com>
2023-08-27 16:47:52 +0530

    🎉 init: initial commit

---
+++ src/utils/AppConfig.ts
@@ -0,0 +1,8 @@
+// FIXME: Update this configuration file based on your project information
+
+export const AppConfig = {
+  site_name: 'Starter',
+  title: 'Next13 Boilerplate',
+  description: 'Starter code for your Next13 Boilerplate with Tailwind CSS',
+  locale: 'en',
+};
