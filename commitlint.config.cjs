67f9d7b7106953445df8340137b96a4fe05c17ba
paalamugan <paalamugan@gmail.com>
2023-08-27 17:50:52 +0530

    👷 ci: fixing ci build

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -2,6 +2,12 @@
 
 module.exports = {
   extends: ['gitmoji'],
+  parserPreset: {
+    parserOpts: {
+      headerPattern: /^(:\w*:)(?:\s)(?:\((.*?)\))?\s((?:.*(?=\())|.*)(?:\(#(\d*)\))?/,
+      headerCorrespondence: ['type', 'scope', 'subject', 'ticket']
+    }
+  },
   rules: {
     'type-enum': [
       2,
